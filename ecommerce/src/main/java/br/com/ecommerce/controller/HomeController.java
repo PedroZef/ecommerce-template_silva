637290a8c0e311494f721ced47c97c7bff8ada8c
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2026-05-18 12:05:44 -0300

    organizando a pasta e package

--- ecommerce/src/main/java/br/com/ecommerce/controller/HomeController.java
+++ ecommerce/src/main/java/br/com/ecommerce/controller/HomeController.java
@@ -1,4 +1,4 @@
-package br.com.ecommerce.silva.controller;
+package br.com.ecommerce.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- ecommerce/src/test/java/br/com/ecommerce/EcommerceApplicationTests.java
+++ ecommerce/src/test/java/br/com/ecommerce/EcommerceApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.ecommerce.silva;
+package br.com.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
